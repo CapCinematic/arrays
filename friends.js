a673df3e352fa4218b9d5136e9d2716c8952fb52
Isaiah Richardson <capcinematic@gmail.com>
2022-11-09 17:44:49 -0700

    Add places

--- friends.js
+++ friends.js
@@ -1,4 +1,7 @@
-arrays=["frank", "lucy", "cody", "tom", "mike"]
+var arrays=["frank", "lucy", "cody", "tom", "mike"]
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+var places=["Paris", "Canada", "Bali", "Goa", "rome"]
+places.pop()
+console.log(places)
